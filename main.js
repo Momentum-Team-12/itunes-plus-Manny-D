d62b88d45137df97ee434bf61abc31f3bda1105c
Manny Dador <EADador@gmail.com>
2022-04-07 17:27:35 -0400

    looks but no funct

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 console.log("Testing")
 
-let profileDiv1 = document.querySelector('#profile')
-
 fetch ('https://itunes.apple.com/search?term=nsyc', {
     method: 'GET',
     headers: {},
@@ -9,25 +7,58 @@ fetch ('https://itunes.apple.com/search?term=nsyc', {
     .then(function(response) {  // response: what you got from code above 
         return response.json()
     })
-    .then (function (data) {  // data is what's retuned in line 10
-        console.log('The response is', data)
+    .then (function (data) {  // data is what's retuned in line 8
+        console.log('The response is', data.results)
 
-        //test - add band/artist thumbnail
-        let imgDiv = document.querySelector('#imgDiv')
-        let bandImage = document.createElement('img')
-        bandImage.classList.add('thumbnail')
-        imgDiv.appendChild(bandImage)
-        bandImage.src = `${data.results[0].artworkUrl100}`
-        imgDiv.appendChild(bandImage)
+        // for (let i of data.results) {
+        //     console.log(i.trackname)
 
-
-        //test - Pulling Data - working (found StackO for data pull)
-        let nameDiv = document.querySelector('#tDiv')
-        let bandName = document.createElement('p')
-        bandName.classList.add('aName')
-        nameDiv.appendChild(bandName)
-        bandName.innerText = `Artist: ${data.results[0].artistName}`
-        nameDiv.appendChild(bandName)
+        // //test - add band/artist thumbnail
+        // let imgDiv = document.querySelector('#imgDiv')
+        // let bandImage = document.createElement('img')
+        // bandImage.classList.add('thumbnail')
+        // imgDiv.appendChild(bandImage)
+        // bandImage.src = `${results.artworkUrl100}`
+        // imgDiv.appendChild(bandImage)
 
 
-})
+
+        // //test - Pulling Data - working (found StackO for data pull)
+        // let nameDiv = document.querySelector('#tDiv')
+        // let bandName = document.createElement('p')
+        // bandName.classList.add('aName')
+        // nameDiv.appendChild(bandName)
+        // bandName.innerText = `Artist: ${results.artistName}`
+        // nameDiv.appendChild(bandName)
+
+        //Test - for search bar
+        const searchContainer = document.querySelector('#search')
+
+
+        const mainContainer = document.querySelector('#bandDivs')  
+
+        let results = data.results.slice(1)
+            for (let result of results) {
+                let bandDiv = document.createElement('div')
+                bandDiv.classList.add('bandPanel')
+
+        let imgThumb = document.createElement('img')  
+            imgThumb.classList.add('bImg')
+            imgThumb.src = result.artworkUrl100 
+            bandDiv.appendChild(imgThumb)
+
+        let artName = document.createElement('p') 
+            artName.classList.add('bName')
+            artName.innerText = result.artistName 
+            bandDiv.appendChild(artName)
+    
+        let songName = document.createElement('p')  
+            songName.classList.add('bSong')
+            songName.innerText = result.trackName 
+            bandDiv.appendChild(songName)
+
+        mainContainer.appendChild(bandDiv)
+
+        }
+
+    })
