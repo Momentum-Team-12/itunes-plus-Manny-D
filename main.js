16163242362458a009cd7692cb04522f5361e12f
Manny Dador <EADador@gmail.com>
2022-04-11 22:43:51 -0400

    tPlayer working/clear funtionality needed

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 console.log("Testing")
 
 // Search Form
-
-
 let form = document.querySelector('#search-form')
 console.log(`This is my search form: ${form}`)
 
@@ -20,7 +18,7 @@ fetch (`https://proxy-itunes-api.glitch.me/search?term=${uInput.value}&media=mus
     .then(function(response) {  // response: what you got from code above 
         return response.json()
     })
-    .then (function (data) {  // data is what's retuned in line 21
+    .then (function (data) {  // data is what's retuned in line 19
         console.log('The response is', data.results)
         // Test - Band Info Div - working
         const mainContainer = document.querySelector('#mainDiv')  
@@ -40,32 +38,48 @@ fetch (`https://proxy-itunes-api.glitch.me/search?term=${uInput.value}&media=mus
                     artName.innerText = result.artistName 
                     bandDiv.appendChild(artName)
     
-                let songName = document.createElement('p')  
-                    songName.classList.add('bSong')
-                    songName.innerText = result.trackName 
-                    bandDiv.appendChild(songName)
+                // Orig working songname that I'll test linking    
+                // let songName = document.createElement('p')  
+                //     songName.classList.add('bSong')
+                //     songName.innerText = result.trackName 
+                //     bandDiv.appendChild(songName)
                     
-                let playAudio = document.createElement("audio");
-                playAudio.classList.add('playAudio')
-                playAudio.controls = true
-                playAudio.src = result.previewUrl
-                playAudio.addEventListener("click", event)
-                bandDiv.appendChild(playAudio)
+                // Song name now linked and opens player at top / have to clear tho
+                let songName = document.createElement('p')  
+                let a = document.createElement('a')
+                a.classList.add('cToPlay')
+                let linkedSong = document.createTextNode(`${result.trackName}`)
+                a.appendChild(linkedSong)
+                a.title = `${result.trackName}`
+                a.href = `${result.previewUrl}`
+                bandDiv.appendChild(a)
+
+                // Working player in each div
+                // let playAudio = document.createElement("audio");
+                // playAudio.classList.add('playAudio')
+                // playAudio.controls = true
+                // playAudio.src = result.previewUrl
+                // playAudio.addEventListener("click", event)
+                // bandDiv.appendChild(playAudio)
 
                 mainContainer.appendChild(bandDiv)
 
-                
+                // Test for top Audio player / working but needs to clear on new
+                const topPlayer = document.querySelector("#player")
+                a.addEventListener("click", function () {
+                let player = document.createElement('audio')
+                player.controls = true
+                player.src = result.previewUrl
+                let trackName = document.createElement('p') 
+                trackName.innerText = `Now Playing: ${result.trackName}`
+                topPlayer.appendChild(player)
+                topPlayer.appendChild(trackName)
+            })
         }
     })
 })
 
+// Test - clear for next search
 
-// Thought for audio: link or button in bandDiv that links to .result.previewUrl that plays on the audio player in HTML with the ID of player
-// let songPreview = 
-
-// mish moshing random code
-// let pagePlayer = document.querySelector("#player");
-// pagePlayer.src = result.previewUrl
-// //https://stackoverflow.com/questions/55998756/need-help-using-javascript-with-htmlplayer/56000290#56000290
-// pagePlayer.addEventListener('click'
-
+// document.querySelector("search").addEventListener('click', event) 
+// document.querySelector("search").innerHTML = '';
