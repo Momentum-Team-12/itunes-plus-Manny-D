afb6f14ce06004e659d1d1a09321d46a4143b0ab
Manny Dador <EADador@gmail.com>
2022-04-13 15:54:18 -0400

    Clear audio and Reload working again

--- main.js
+++ main.js
@@ -1,112 +1,107 @@
-console.log("Connected")
+console.log("Connected");
 
 // Search Form
-let form = document.querySelector('#search-form')
+let form = document.querySelector("#search-form");
 
-form.addEventListener('submit', function(event) {
-event.preventDefault()
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-// Reloads / refreshes mainDiv 
-function adiosMD (parent) {
+  // Reloads / refreshes mainDiv
+  function adiosMD(parent) {
     while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+      parent.removeChild(parent.firstChild);
     }
-}
-adiosMD(mainDiv)
+  }
+  adiosMD(mainDiv);
 
+  let uInput = document.querySelector("#search-input");
 
-let uInput = document.querySelector('#search-input')
-
-
-fetch (`https://proxy-itunes-api.glitch.me/search?term=${uInput.value}&media=music`, {
-    method: 'GET',
-    headers: {},
-})
-    .then(function(response) {  // Response: what you got from code above 
-        return response.json()
+  fetch(
+    `https://proxy-itunes-api.glitch.me/search?term=${uInput.value}&media=music`,
+    {
+      method: "GET",
+      headers: {},
+    }
+  )
+    .then(function (response) {
+      // Response: what you got from code above
+      return response.json();
     })
-    .then (function (data) {  // Data is what's retuned in line 26
-        // console.log('The response is', data.results)
+    .then(function (data) {
+      // Data is what's retuned in line 28
+      // console.log('The response is', data.results)
 
-            if (data.results) {
-                if (data.results.length === 0){
-                    console.log('No Results Found') // For 'error' logging
-                    return 
-                }
-                
+      if (data.results) {
+        if (data.results.length === 0) {
+          console.log("No Results Found"); // For 'error' logging
+          let error = document.createElement("p");
+          error.classList.add("error");
+          error.innerText = "Error Searching.. please try again.";
+          mainDiv.appendChild(error);
+          return;
+        }
 
         // Band Info Div - working
-        const mainContainer = document.querySelector('#mainDiv')  
+        const mainContainer = document.querySelector("#mainDiv");
 
-        let results = data.results.slice(1)
-            for (let result of results) {
-                let bandDiv = document.createElement('div')
-                    bandDiv.classList.add('bandPanel')
+        let results = data.results.slice(1);
+        for (let result of results) {
+          let bandDiv = document.createElement("div");
+          bandDiv.classList.add("bandPanel");
 
-                let imgThumb = document.createElement('img')  
-                    imgThumb.classList.add('bImg')
-                    imgThumb.src = result.artworkUrl100 
-                    bandDiv.appendChild(imgThumb)
+          let imgThumb = document.createElement("img");
+          imgThumb.classList.add("bImg");
+          imgThumb.src = result.artworkUrl100;
+          bandDiv.appendChild(imgThumb);
 
-                let artName = document.createElement('p') 
-                    artName.classList.add('bName')
-                    artName.innerText = result.artistName 
-                    bandDiv.appendChild(artName)
-                    
+          let artName = document.createElement("p");
+          artName.classList.add("bName");
+          artName.innerText = result.artistName;
+          bandDiv.appendChild(artName);
 
-                // Song name now linked and opens player at top
-                let songName = document.createElement('p')  
-                let a = document.createElement('a')
-                a.classList.add('cToPlay')
-                let linkedSong = document.createTextNode(`${result.trackName}`)
-                a.appendChild(linkedSong)
-                a.title = `${result.trackName}`
-                a.href = `${result.previewUrl}`
-                bandDiv.appendChild(a)
-                let clickMe = document.createElement('p')
-                clickMe.innerText = "Click song title above to hear a preview!"
-                bandDiv.appendChild(clickMe)
+          // Song name now linked and opens player at top
+          let songName = document.createElement("p");
+          let track = document.createElement("a");
+          track.classList.add("cToPlay");
 
-                mainContainer.appendChild(bandDiv)
-            
+          let linkedSong = document.createTextNode(`${result.trackName}`);
+          track.appendChild(linkedSong);
+          // track.title = `${result.trackName}`
+          track.dataset.href = `${result.previewUrl}`;
+          bandDiv.appendChild(track);
 
-                // Test for top Audio player / gross but working and clears
-                const topPlayer = document.querySelector("#player")
-                // document.getElementById("player").style.height = "190px";
-                a.addEventListener("click", function () {
-                    if (topPlayer.innerHTML === "") {
-                        let player = document.createElement('audio')
-                        player.controls = true
-                        // player.controlsList= "nodownload"
-                        player.src = result.previewUrl
-                        player.play()
-                        // player.prop('preload','none')
-                        let trackName = document.createElement('p') 
-                        trackName.innerText = `Now Listening to: ${result.trackName}`
-                        // topPlayer.appendChild(imgThumb)
-                        topPlayer.appendChild(player)
-                        topPlayer.appendChild(trackName)
-                    } else {
-                        if (topPlayer.innerHTML !== "") {
-                        topPlayer.innerHTML = ""
-                        uInput.value = "" 
-                        let player = document.createElement('audio')
-                        player.controls = true
-                        // player.controlsList= "nodownload"
-                        player.src = result.previewUrl
-                        player.play()
-                        // player.prop('preload','none')
-                        let trackName = document.createElement('p') 
-                        trackName.innerText = `Now Listening to: ${result.trackName}`
-                        // topPlayer.appendChild(imgThumb)
-                        topPlayer.appendChild(player)
-                        topPlayer.appendChild(trackName)
-                    } 
-                
-                }
-            })
-        }
+          let clickMe = document.createElement("p");
+          clickMe.innerText = "Click song title above to hear a preview!";
+          bandDiv.appendChild(clickMe);
+
+          mainContainer.appendChild(bandDiv);
+
+          // Test for top Audio player / gross / adding player but not clearing
+          const topPlayer = document.querySelector("#player");
+          track.addEventListener("click", function () {
+            function playIT() {
+              let player = document.createElement("audio");
+              player.controls = true;
+              player.src = result.previewUrl;
+              player.play();
+
+              let trackName = document.createElement("p");
+              trackName.innerText = `Now Listening to: ${result.trackName}`;
+              topPlayer.appendChild(player);
+              topPlayer.appendChild(trackName);
             }
-    })
 
-})
+            if (topPlayer.innerHTML === "") {
+              playIT();
+            } else {
+              if (topPlayer.innerHTML !== "") {
+                topPlayer.innerHTML = "";
+                uInput.value = "";
+                playIT();
+              }
+            }
+          });
+        }
+      }
+    });
+});
